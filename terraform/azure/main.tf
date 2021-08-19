cde74ce2da9140636e111d16516488b05d336251
VILLAN3LL3 <development@gutfrau.de>
2021-08-19 21:53:41 +0200

    removed public network access

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -108,7 +108,6 @@ resource "azurerm_postgresql_server" "arpa" {
   geo_redundant_backup_enabled = false
   auto_grow_enabled            = true
 
-  public_network_access_enabled    = false
   ssl_enforcement_enabled          = true
   ssl_minimal_tls_version_enforced = "TLS1_2"
 }
